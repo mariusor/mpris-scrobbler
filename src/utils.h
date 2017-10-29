a7807c74718742466440e5985b9deff08b4f9c75
Marius Orcsik <marius@habarnam.ro>
2017-10-29 12:00:08 +0100

    Using stack memory for logging instead of dynamic allocated

--- src/utils.h
+++ src/utils.h
@@ -87,26 +87,21 @@ static int _log(log_level level, const char* format, ...)
     va_start(args, format);
 
     const char *label = get_log_level(level);
-    size_t l_len = strlen(LOG_WARNING_LABEL);
+    size_t p_len = strlen(LOG_WARNING_LABEL) + 1;
 
-    size_t p_len = l_len + 1;
-    char* preffix = get_zero_string(p_len);
-    snprintf(preffix, p_len + 1, "%-7s ", label);
-
-    char* suffix = "\n";
+    const char* suffix = "\n";
     size_t s_len = strlen(suffix);
     size_t f_len = strlen(format);
-    size_t full_len = p_len + f_len + s_len;
+    size_t full_len = p_len + f_len + s_len + 1;
 
-    char* log_format = get_zero_string(full_len);
-    strncpy(log_format, preffix, p_len);
-    free(preffix);
+    char log_format[full_len];
+    memset(log_format, 0x0, full_len);
+    snprintf(log_format, p_len + 1, "%-7s ", label);
 
     strncpy(log_format + p_len, format, f_len);
     strncpy(log_format + p_len + f_len, suffix, s_len);
 
     int result = vfprintf(stderr, log_format, args);
-    free(log_format);
     va_end(args);
 
     return result;
