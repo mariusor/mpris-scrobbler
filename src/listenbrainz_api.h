3755e219da4f653f0d2fd22b39af81961ba273c3
Marius Orcsik <marius@habarnam.ro>
2017-11-28 10:34:01 +0100

    Adding listenbrainz API file

---
+++ src/listenbrainz_api.h
@@ -0,0 +1,8 @@
+/**
+ * @author Marius Orcsik <marius@habarnam.ro>
+ */
+#ifndef MPRIS_SCROBBLER_LISTENBRAINZ_API_H
+#define MPRIS_SCROBBLER_LISTENBRAINZ_API_H
+
+
+#endif // MPRIS_SCROBBLER_LISTENBRAINZ_API_H
