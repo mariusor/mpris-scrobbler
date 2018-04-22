29985e5f1eb4bab0c11fd8b90f7409d5e62f68b2
Marius Orcsik <marius@habarnam.ro>
2018-04-22 11:33:27 +0200

    Added test for decreasing the size of a stretcy buff

--- tests/stretchy_buffers_test.c
+++ tests/stretchy_buffers_test.c
@@ -41,6 +41,43 @@ describe(stretchy_buffers, {
         sb_free(arr);
     });
 
+    it ("Popping items from a stretchy buffer", {
+        char** arr = NULL;
+
+        asserteq(arr, NULL);
+        asserteq(sb_count(arr), 0);
+
+        for(int i = 0; i < 10; i++) {
+            char *new_el = "aaaaa";
+            sb_push(arr, new_el);
+            assertneq(new_el, NULL);
+        }
+        // checking array reached desired length
+        asserteq(sb_count(arr), 10);
+
+        // checking shrinking by one element
+        char **new_arr = sb_add(arr, (-1));
+        asserteq(sb_count(arr), 9);
+        assertneq(new_arr, NULL);
+
+        // checking shrinking by one element
+        new_arr = sb_add(arr, (-1));
+        asserteq(sb_count(arr), 8);
+        assertneq(new_arr, NULL);
+
+        // checking shrinking by one element
+        new_arr = sb_add(arr, (-1));
+        asserteq(sb_count(arr), 7);
+        assertneq(new_arr, NULL);
+
+        // checking shrinking to zero elements
+        new_arr = sb_add(arr, (-7));
+        asserteq(sb_count(arr), 0);
+        assertneq(new_arr, NULL);
+
+        sb_free(arr);
+    });
+
 });
 
 snow_main();
