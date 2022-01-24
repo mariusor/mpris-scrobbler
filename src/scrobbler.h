db137b45dcb3caab7bc5c37648b55a81762356b4
Marius Orcsik <marius@habarnam.ro>
2022-01-24 14:17:17 +0100

    Try to add extra debug for curl

--- src/scrobbler.h
+++ src/scrobbler.h
@@ -54,6 +54,97 @@ struct scrobbler_connection *scrobbler_connection_new(void)
     return (s);
 }
 
+#if DEBUG
+static void dump(const char *text, FILE *stream, unsigned char *ptr, size_t size, char nohex)
+{
+  size_t i;
+  size_t c;
+
+  unsigned int width = 0x10;
+
+  if(nohex)
+    /* without the hex output, we can fit more on screen */
+    width = 0x40;
+
+  fprintf(stream, "%s, %10.10lu bytes (0x%8.8lx)\n",
+          text, (unsigned long)size, (unsigned long)size);
+
+  for(i = 0; i<size; i += width) {
+
+    fprintf(stream, "%4.4lx: ", (unsigned long)i);
+
+    if(!nohex) {
+      /* hex not disabled, show it */
+      for(c = 0; c < width; c++)
+        if(i + c < size)
+          fprintf(stream, "%02x ", ptr[i + c]);
+        else
+          fputs("   ", stream);
+    }
+
+    for(c = 0; (c < width) && (i + c < size); c++) {
+      /* check for 0D0A; if found, skip past and start a new line of output */
+      if(nohex && (i + c + 1 < size) && ptr[i + c] == 0x0D &&
+         ptr[i + c + 1] == 0x0A) {
+        i += (c + 2 - width);
+        break;
+      }
+      fprintf(stream, "%c",
+              (ptr[i + c] >= 0x20) && (ptr[i + c]<0x80)?ptr[i + c]:'.');
+      /* check again for 0D0A, to avoid an extra \n if it's at width */
+      if(nohex && (i + c + 2 < size) && ptr[i + c + 1] == 0x0D &&
+         ptr[i + c + 2] == 0x0A) {
+        i += (c + 3 - width);
+        break;
+      }
+    }
+    fputc('\n', stream); /* newline */
+  }
+  fflush(stream);
+}
+
+struct data {
+    char trace_ascii;
+};
+
+static int my_trace(CURL *handle, curl_infotype type, char *data, size_t size, void *userp)
+{
+    struct data *config = (struct data *)userp;
+    const char *text;
+    (void)handle; /* prevent compiler warning */
+
+    switch(type) {
+    case CURLINFO_TEXT:
+        fprintf(stderr, "== Info: %s", data);
+        /* FALLTHROUGH */
+    default: /* in case a new one is introduced to shock us */
+        return 0;
+
+    case CURLINFO_HEADER_OUT:
+        text = "=> Send header";
+        break;
+    case CURLINFO_DATA_OUT:
+        text = "=> Send data";
+        break;
+    case CURLINFO_SSL_DATA_OUT:
+        text = "=> Send SSL data";
+        break;
+    case CURLINFO_HEADER_IN:
+        text = "<= Recv header";
+        break;
+    case CURLINFO_DATA_IN:
+        text = "<= Recv data";
+        break;
+    case CURLINFO_SSL_DATA_IN:
+        text = "<= Recv SSL data";
+        break;
+    }
+
+    dump(text, stderr, (unsigned char *)data, size, config->trace_ascii);
+    return 0;
+}
+#endif
+
 static void event_cb(int, short, void *);
 void scrobbler_connection_init(struct scrobbler_connection *connection, struct scrobbler *s, struct api_credentials *credentials, int idx)
 {
@@ -67,6 +158,16 @@ void scrobbler_connection_init(struct scrobbler_connection *connection, struct s
     connection->parent = s;
     connection->ev = event_new(s->evbase, connection->sockfd, 0, event_cb, s);
     _trace("scrobbler::connection_init[%s:%d:%p]:curl_easy_handle(%p)", get_api_type_label(credentials->end_point), idx, connection, connection->handle);
+
+#if DEBUG
+    struct data config = {.trace_ascii = 1};
+
+    curl_easy_setopt(connection->handle, CURLOPT_DEBUGFUNCTION, my_trace);
+    curl_easy_setopt(connection->handle, CURLOPT_DEBUGDATA, &config);
+    /* the DEBUGFUNCTION has no effect until we enable VERBOSE */
+    curl_easy_setopt(connection->handle, CURLOPT_VERBOSE, 1L);
+#endif
+    curl_easy_setopt(connection->handle, CURLOPT_FOLLOWLOCATION, 1L);
 }
 
 static void scrobbler_clean(struct scrobbler *s)
